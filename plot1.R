b89c5907005845cbc9befd7c06ec96f22f4198d0
kaiyang1995 <kaiyang1995@gmail.com>
2020-06-13 18:39:19 +0800

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+png(file="plot1.png",width=480,height=480)
+hist(data1$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
+dev.off()
